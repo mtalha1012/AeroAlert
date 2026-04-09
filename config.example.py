141d4b75598245078967a4aaf08e52036cf32152
mtalha1012 <mtalha.101206@gmail.com>
2026-04-10 00:37:14 +0500

    Add files via upload

---
+++ config.example.py
@@ -0,0 +1,8 @@
+# config.example.py
+EMAIL = "your_nust_email@seecs.edu.pk"
+APP_PASSWORD = "your_app_password"
+GEMINI_API_KEY = "your_gemini_api_key"
+DECISION_PROMPT = ""
+CAPTION_PROMPT = ""
+ALLOWED_SENDERS = {}
+TEACHERS = {}
