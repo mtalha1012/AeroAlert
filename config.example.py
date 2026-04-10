dbd601edd9274f48a48ea34838b6daabb1606187
mtalha1012 <mtalha.101206@gmail.com>
2026-04-10 14:01:32 +0500

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -2,6 +2,7 @@
 EMAIL = "your_nust_email@seecs.edu.pk"
 APP_PASSWORD = "your_app_password"
 GEMINI_API_KEY = "your_gemini_api_key"
+CONTACT_NAME = "your contact name"
 DECISION_PROMPT = ""
 CAPTION_PROMPT = ""
 ALLOWED_SENDERS = {}
